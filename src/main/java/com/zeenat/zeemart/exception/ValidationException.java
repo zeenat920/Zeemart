a361ccdf1f1a617fa16cf015a3c0264405789aa9
zeenat920 <meeranmeeran3888@gmail.com>
2026-08-15 11:13:18 +0530

    Create ValidationException.java

---
+++ src/main/java/com/zeenat/zeemart/exception/ValidationException.java
@@ -0,0 +1,14 @@
+package com.zeenat.zeemart.exception;
+
+public class ValidationException extends Exception {
+    private final String field;
+
+    public ValidationException(String field, String message) {
+        super(message);
+        this.field = field;
+    }
+
+    public String getField() {
+        return field;
+    }
+}
