27bca314750c7ebf59c0e623d933fd46c6d88c42
zeenat920 <meeranmeeran3888@gmail.com>
2026-08-15 11:29:47 +0530

    Add EncodingFilter to set UTF-8 character encoding

---
+++ src/main/java/com/zeenat/zeemart/filter/EncodingFilter.java
@@ -0,0 +1,15 @@
+package com.zeenat.zeemart.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class EncodingFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        req.setCharacterEncoding("UTF-8");
+        res.setCharacterEncoding("UTF-8");
+        chain.doFilter(req, res);
+    }
+}
