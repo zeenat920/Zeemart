d44be8202f48c26877f9af12106b02ec331a1562
zeenat920 <meeranmeeran3888@gmail.com>
2026-08-16 12:40:35 +0530

    Update ProductDAO.java

--- src/main/java/com/zeenat/zeemart/dao/ProductDAO.java
+++ src/main/java/com/zeenat/zeemart/dao/ProductDAO.java
@@ -1,6 +1,7 @@
 package com.zeenat.zeemart.dao;
 
 import com.zeenat.zeemart.model.Product;
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
@@ -9,5 +10,4 @@ public interface ProductDAO {
     List<Product> search(String keyword, String category) throws SQLException;
     Optional<Product> findById(int id) throws SQLException;
     void decrementStock(int productId, int qty, Connection conn) throws SQLException;
-    interface Connection extends java.sql.Connection {}
 }
